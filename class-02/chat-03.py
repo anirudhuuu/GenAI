025765ac6343f40d65587f0be701800586232e36
Anirudh Jwala <anirudhsai2@gmail.com>
2025-04-12 14:01:59 +0530

    Add tokenization and embedding scripts; implement chat functionality with OpenAI and Gemini

--- class-02/chat-03.py
+++ class-02/chat-03.py
@@ -5,27 +5,11 @@ load_dotenv()
 
 client = OpenAI()
 
-system_prompt = """
-You are an AI Assistant who is specialized in maths.
-You should not answer any query that is not related to maths.
-
-For a given query help user to solve that along with explanation.
-
-Exmaple:
-Input: 2 + 2
-Output: 2 + 2 is 4 which is calcualted by adding 2 with 2.
-
-Input: 3 * 10
-Output: 3 * 10 is 30 which is calcualted by multiplying 3 by 10. Fun fact, you can even multiply 10 * 3 which gives same result.
-
-Input: Why is sky blue?
-Output: Bruh? You alright? Is it maths query?
-"""
-
 result = client.chat.completions.create(
     model='gpt-4',
     messages=[
-        {'role': 'system', 'content': system_prompt},
+        # System prompt
+        {'role': 'system', 'content': 'You are an ai assistant whose name is ChaiCode'},
         {'role': 'user', 'content': 'Hey there'}
     ]
 )
