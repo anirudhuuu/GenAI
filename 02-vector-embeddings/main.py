1275e659b6ddbc2e0c8b0c75c74fdfc03f1a0dc4
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-28 12:28:58 +0530

    feat: update vector embeddings generation to use new API key and model version

--- 02-vector-embeddings/main.py
+++ 02-vector-embeddings/main.py
@@ -1,16 +1,21 @@
-from dotenv import load_dotenv
+import os
 from openai import OpenAI
 
-load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
-client = OpenAI()
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 text = "dog chases cat"
 
 response = client.embeddings.create(
-    model="text-embedding-3-small",
+    model="text-embedding-004",
     input=text
 )
 
 print("Vector Embeddings", response)
+
+# Indicator for no. of dimensions for vector embedding
 print("Length", len(response.data[0].embedding))
