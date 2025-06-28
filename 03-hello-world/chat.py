a6a1d05e4d9f04409cd60ec5b3606930e37e4971
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-28 12:40:53 +0530

    feat: enhance chat.py with additional examples and context handling for AI interactions

--- 03-hello-world/chat.py
+++ 03-hello-world/chat.py
@@ -8,10 +8,71 @@ client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
+"""
+Simple chat with AI
+=====================
 response = client.chat.completions.create(
     model="gemini-2.0-flash",
     messages=[
         {"role": "user", "content": "Hey, there"}
+    ]
+)
+"""
+
+"""
+No access to real-time data
+==============================
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "user", "content": "What is the weather today?"}
+    ]
+)
+"""
+
+"""
+Stateless responses
+==============================
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "user", "content": "Hey, my name is Anirudh"}
+    ]
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "user", "content": "Whats my name?"}
+    ]
+)
+
+No access to your history, unless provided
+"""
+
+"""
+Provide with history and context
+====================================
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "user", "content": "Hey, my name is Anirudh"},
+        {"role": "assistant",
+            "content": "Hi Anirudh, it's nice to meet you! How can I help you today?"},
+        {"role": "user", "content": "Whats my name?"}
+    ]
+)
+"""
+
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "user", "content": "Hey, my name is Anirudh"},
+        {"role": "assistant",
+            "content": "Hi Anirudh, it's nice to meet you! How can I help you today?"},
+        {"role": "user", "content": "Whats my name?"},
+        {"role": "assistant", "content": "Your name is Anirudh."},
+        {"role": "user", "content": "How are you?"},
     ]
 )
 
